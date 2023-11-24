ada59f12c975982247f5b89da20183b28c13be5e
Nikita Khomitsevych <hamsternik9@gmail.com>
2023-11-24 00:35:28 +0000

    add search

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
       colors: {
+        primary:  {
+          main: '#C3ACD0',
+          dark: '#7743DB',
+          light: '#F7EFE5',
+        },
         blue: {
           400: '#2589FE',
           500: '#0070F3',
